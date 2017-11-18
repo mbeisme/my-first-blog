7fb07a1f1bf1e7089bad77d2ac0ba2d943d28423
mbeisme <matiaseusterbrock@gmail.com>
2017-11-18 13:31:28 -0800

    My 2nd commit

--- blog/models.py
+++ blog/models.py
@@ -17,19 +17,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-class Post2(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=100)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-        default=timezone.now)
-    published_date = models.DateTimeField(
-        blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
